018d56991685984066aedb0152ced676b0340edc
Felix <felix.reinemann@gmail.com>
2018-09-05 20:19:42 +0200

    Changed to new Unity ProjectFiles

---
+++ VR_Leichenschau/Assets/Scripts/GrabableObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(FixedJoint))]
+public class GrabableObject : MonoBehaviour {
+	
+	public FixedJoint spring;
+	public Rigidbody connectedTo;
+	Rigidbody myRigid;
+	// Use this for initialization
+	void Start () {
+		myRigid = GetComponent<Rigidbody>();
+		spring = GetComponent<FixedJoint>();
+	}
+	
+	void Update(){
+		if(connectedTo!=null){
+			//spring.connectedAnchor = connectedTo.position;
+		}
+	}
+
+	public void OnGrab(Rigidbody hand){
+		Debug.Log("grab got called");
+		connectedTo = hand;
+		spring.connectedBody = hand;
+		myRigid.useGravity = false;
+		//myRigid.isKinematic = true;
+		//spring.connectedAnchor = grabbedTo.position;
+	}
+
+	public void Release(){
+		connectedTo = null;
+		spring.connectedBody = null;
+		myRigid.useGravity = true;
+		//myRigid.isKinematic = false;
+	}
+}
---
+++ VR_Leichenschau/Assets/Scripts/OTouchPickUp.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OTouchPickUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		mask = 1 << LayerMask.NameToLayer("Interactable");
+		col = GetComponent<SphereCollider>();
+		myRigid = GetComponent<Rigidbody>();
+	}
+	
+	public bool grabbing;
+	public SphereCollider col;
+	Rigidbody myRigid;
+	GameObject grabbedObject;
+	// Update is called once per frame
+
+	RaycastHit hit;
+	int mask;
+	void Update () {
+		OVRInput.Update();
+		if(OVRInput.Get( OVRInput.Axis1D.PrimaryHandTrigger) > 0 || OVRInput.Get( OVRInput.Axis1D.SecondaryHandTrigger) > 0){
+			grabbing = true;
+			//Debug.Log(OVRInput.Get( OVRInput.Axis1D.PrimaryHandTrigger) +" ; "+OVRInput.Get( OVRInput.Axis1D.SecondaryHandTrigger));
+			if(grabbedObject == null){
+				Collider[] colObjects = Physics.OverlapSphere(transform.position,col.radius,mask);
+				Debug.Log("Amount: "+colObjects.Length);
+				for (int i = 0; i < colObjects.Length; i++){
+					colObjects[i].attachedRigidbody.GetComponent<GrabableObject>().OnGrab(myRigid);
+					grabbedObject = colObjects[i].attachedRigidbody.gameObject;
+					break;
+				}
+			}
+		} else {
+			grabbing = false;
+			if(grabbedObject != null){
+				grabbedObject.GetComponent<GrabableObject>().Release();
+				grabbedObject = null;
+			}
+		}
+	}
+}
