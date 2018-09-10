aa51ddde987706c0f99617dad6fdb339400b3a06
Felix <felix.reinemann@gmail.com>
2018-09-10 14:14:40 +0200

    added rotationcontrols

--- VR_Leichenschau/Assets/Scripts/VRMove.cs
+++ VR_Leichenschau/Assets/Scripts/VRMove.cs
@@ -9,8 +9,10 @@ public class VRMove : MonoBehaviour
     public float rotationSpeed;
 
     public Vector2 moveStick;
+    public Vector2 rotateStick;
     Vector3 moveVector;
     public Transform head;
+    public Transform toRotate;
     Quaternion myRotation;
     // Use this for initialization
     void Start()
@@ -23,10 +25,18 @@ public class VRMove : MonoBehaviour
     {
         OVRInput.Update();
         moveStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
-        myRotation = Quaternion.LookRotation(new Vector3(head.forward.x, 0, head.forward.z).normalized, Vector3.up);
-        moveVector = myRotation * (transform.forward * moveStick.y + transform.right * moveStick.x);
-        transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+        rotateStick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        //---------movement---------------//
+        //myRotation = Quaternion.LookRotation(new Vector3(head.forward.x, 0, head.forward.z).normalized, Vector3.up);
+        //moveVector = myRotation * (transform.forward * moveStick.y + transform.right * moveStick.x);
+        moveVector = (transform.forward * moveStick.y + transform.right * moveStick.x);
+        moveVector = new Vector3(moveVector.x,0,moveVector.z);
 
 
+        //transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+        //transform.Translate(moveVector * moveSpeed * Time.deltaTime,Space.Self);
+        transform.position = transform.position + moveVector* moveSpeed * Time.deltaTime;
+        //--------------rotation--------------//
+        toRotate.RotateAround(head.position, Vector3.up, Time.deltaTime * rotationSpeed * rotateStick.x);
     }
 }
