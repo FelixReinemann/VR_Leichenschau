2879b93d0e58a3a5f5e79ff26088e704935adf17
Felix <felix.reinemann@gmail.com>
2018-11-17 13:59:37 +0100

    Added basic script to call new Menus - first draft of Temperaturemenu added

---
+++ VR_Leichenschau/Assets/Scripts/Menu/MenuButton_SpawnNewMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton_SpawnNewMenu : MenuButton {
+
+	public GameObject objectToEnable;
+	public bool closeMyMenu=true;
+	public bool disableButtonAfterAction=false;
+	public override void OnClick(){
+		objectToEnable.SetActive(true);
+		objectToEnable.GetComponent<MyMenu>().UnParent(transform.position);
+		if(closeMyMenu)closeMenu();
+		if(disableButtonAfterAction)disableButtonInMenu();
+	}
+}
