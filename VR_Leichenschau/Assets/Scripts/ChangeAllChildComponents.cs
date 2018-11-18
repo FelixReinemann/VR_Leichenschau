02a43badc6b3917c0a3d9c93ddc679bd96a3ac8d
Felix <felix.reinemann@gmail.com>
2018-11-18 08:00:23 +0100

    Added script to masschange rigidbody properties - gave bodies drag and angular drag for slightly better movementbehaviour

---
+++ VR_Leichenschau/Assets/Scripts/ChangeAllChildComponents.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ChangeAllChildComponents : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UpdateMyRigidsArray();
+		UpdateMyRigidsData();
+	}
+	
+	Rigidbody[] myRigids;
+	public float mass=1f, drag, dragTorque;
+
+	void Update () {
+		if(myRigids.Length==0)return;
+		if(mass!=myRigids[0].mass || drag != myRigids[0].drag || dragTorque != myRigids[0].angularDrag){
+			UpdateMyRigidsData();
+		}
+	}
+
+	void UpdateMyRigidsArray(){
+		myRigids = GetComponentsInChildren<Rigidbody>();
+	}
+
+	void UpdateMyRigidsData(){
+		foreach(Rigidbody rig in myRigids){
+			rig.mass = mass;
+			rig.drag = drag;
+			rig.angularDrag = dragTorque;
+		}
+	}
+}
